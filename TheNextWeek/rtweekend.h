f6b638ffcf779d309b36e2dfd4fc4f0aa5ad1000
JDFZ <765422804@qq.com>
2025-10-04 11:15:39 +0800

    init next week

---
+++ TheNextWeek/rtweekend.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <cmath>
+#include <limits>
+#include <memory>
+#include <cstdlib>
+#include <iostream>
+
+// c++ std using
+using std::shared_ptr;
+using std::make_shared;
+
+// constant
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.14159265358979323846;
+
+// utility functions
+
+inline double degrees_to_radians(double degrees) 
+{
+    return degrees * pi / 180.0;
+}
+
+inline double random_double()
+{
+    // [0, 1) range
+    return std::rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max)
+{
+    // [min, max) range
+    return min + (max - min) * random_double();
+}
+
+// common headers
+
+#include "vec3.h"
+#include "ray.h"
+#include "color.h"
